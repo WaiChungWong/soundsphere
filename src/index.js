62d40983725ebac50aebc62f0387d5c01ad7743e
John Wong <johnwongwwc@gmail.com>
2018-07-31 23:59:16 +0800

    removed registerserviceworker

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import React from "react";
 import { render } from "react-dom";
 
-import registerServiceWorker from "./registerServiceWorker";
-
 import App from "./app";
 
 render(<App />, document.getElementById("root"));
-
-registerServiceWorker();
